0e6462d57811d88a0339c8ec4f7350e3ecb4bb96
Daviiiiiddddd <160326963+Daviiiiiddddd@users.noreply.github.com>
2026-01-19 21:48:57 +0200

    Update FolderLock.bat

--- FolderLock.bat
+++ FolderLock.bat
@@ -3,78 +3,61 @@ title Secure Folder Manager - By Mlakot
 cls
 color 0A
 
+:: التأكد من وجود المجلد المخفي أو المجلد العادي
 :MAIN
+cls
 echo ================================
-echo    Secure Folder Manager
+echo     Secure Folder Manager
 echo      Created by MLakot
 echo ================================
 echo.
+
 if EXIST "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" goto UNLOCK
-if NOT EXIST Private goto OPTIONS
+if EXIST Private goto OPTIONS_LOCKED
+if NOT EXIST Private goto CREATE_NEW
 
-echo [1] Lock Folder
-echo [2] Create New Private Folder
-echo [3] Exit
+:OPTIONS_LOCKED
+echo [1] Lock Folder (Private)
+echo [2] Exit
 echo.
-choice /c 123 /n /m "Select option: "
-
-if errorlevel 3 goto END
-if errorlevel 2 goto CREATE
-if errorlevel 1 goto LOCK
+set /p opt="Select option: "
+if "%opt%"=="1" goto LOCK
+if "%opt%"=="2" goto END
+goto MAIN
 
 :LOCK
-cls
-echo Locking Private Folder...
-timeout /t 2 /nobreak >nul
 ren Private "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 attrib +h +s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 echo ✓ Folder locked successfully!
-timeout /t 2 /nobreak >nul
+pause
 goto MAIN
 
 :UNLOCK
-cls
 echo ================================
-echo      UNLOCK SECURE FOLDER
+echo       UNLOCK SECURE FOLDER
 echo ================================
-echo.
 set /p "pass=Enter password: "
-if "%pass%"=="YOUR_PASSWORD_HERE" goto SUCCESS_UNLOCK
+:: ضع باسووردك هنا مكان YOUR_PASSWORD
+if NOT "%pass%"=="PASSWORD" goto FAIL
 
-echo ✗ Invalid password!
-timeout /t 2 /nobreak >nul
-goto MAIN
-
-:SUCCESS_UNLOCK
 attrib -h -s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 ren "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" Private
 echo ✓ Folder unlocked successfully!
-timeout /t 2 /nobreak >nul
+pause
 goto MAIN
 
-:CREATE
-cls
-if EXIST Private (
-    echo ! Private folder already exists!
-    timeout /t 2 /nobreak >nul
-    goto MAIN
-)
-md Private
-echo ✓ Private folder created successfully!
-timeout /t 2 /nobreak >nul
+:FAIL
+echo ✗ Invalid password!
+pause
 goto MAIN
 
-:OPTIONS
-echo.
-echo [1] Create Private Folder
-echo [2] Back to Main
-choice /c 12 /n /m "Select option: "
-if errorlevel 2 goto MAIN
-if errorlevel 1 goto CREATE
+:CREATE_NEW
+echo [1] Create New Private Folder
+echo [2] Exit
+set /p opt="Select option: "
+if "%opt%"=="1" md Private && echo Created! && pause && goto MAIN
+if "%opt%"=="2" goto END
+goto MAIN
 
 :END
-cls
-echo Thank you for using Secure Folder Manager!
-echo Created by Mlakot
-timeout /t 2 /nobreak >nul
 exit
